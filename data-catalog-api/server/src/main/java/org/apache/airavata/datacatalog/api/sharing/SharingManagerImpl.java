5fbb51088f359d3b3a7800faebc01a28cd5b9dce
Isuru Ranawaka <irjanith@gmail.com>
2023-02-14 15:01:30 -0500

    implement sharing logic

--- data-catalog-api/server/src/main/java/org/apache/airavata/datacatalog/api/sharing/SharingManagerImpl.java
+++ data-catalog-api/server/src/main/java/org/apache/airavata/datacatalog/api/sharing/SharingManagerImpl.java
@@ -5,16 +5,22 @@ import org.apache.airavata.datacatalog.api.GroupInfo;
 import org.apache.airavata.datacatalog.api.Permission;
 import org.apache.airavata.datacatalog.api.UserInfo;
 import org.apache.airavata.datacatalog.api.model.UserEntity;
-
+import org.apache.custos.sharing.core.exceptions.CustosSharingException;
 import org.apache.custos.sharing.core.impl.SharingImpl;
+import org.apache.custos.sharing.core.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
-public class SharingManagerImpl  implements SharingManager {
+public class SharingManagerImpl implements SharingManager {
 
     @Autowired
     SharingImpl custosSharingImpl;
+
+    private static final String PUBLIC_ACCESS_GROUP = "public_access_group";
 
 
     @Override
@@ -23,8 +29,9 @@ public class SharingManagerImpl  implements SharingManager {
     }
 
     @Override
-    public boolean userHasAccess(UserInfo userInfo, DataProduct dataProduct, Permission permission) {
-        return false;
+    public boolean userHasAccess(UserInfo userInfo, DataProduct dataProduct, Permission permission) throws CustosSharingException {
+        return custosSharingImpl.userHasAccess(userInfo.getTenantId(), dataProduct.getDataProductId(), permission.name(),
+                userInfo.getUserId());
     }
 
     @Override
@@ -33,37 +40,63 @@ public class SharingManagerImpl  implements SharingManager {
     }
 
     @Override
-    public void grantPermissionToUser(UserInfo userInfo, DataProduct dataProduct, Permission permission) {
+    public void grantPermissionToUser(UserInfo userInfo, DataProduct dataProduct, Permission permission) throws CustosSharingException {
+
+        List<String> userIds = new ArrayList<>();
+        userIds.add(userInfo.getUserId());
+        custosSharingImpl.shareEntity(userInfo.getTenantId(),
+                dataProduct.getDataProductId(), permission.name(), userIds, true, Constants.USER, null);
+    }
+
+    @Override
+    public void revokePermissionFromUser(UserInfo userInfo, DataProduct dataProduct, Permission permission) throws CustosSharingException {
+
+        List<String> userIds = new ArrayList<>();
+        userIds.add(userInfo.getUserId());
+        custosSharingImpl.revokePermission(userInfo.getTenantId(),
+                dataProduct.getDataProductId(), permission.name(), userIds);
 
     }
 
     @Override
-    public void revokePermissionFromUser(UserInfo userInfo, DataProduct dataProduct, Permission permission) {
+    public void grantPermissionToGroup(GroupInfo groupInfo, DataProduct dataProduct, Permission permission) throws CustosSharingException {
+
+        List<String> userIds = new ArrayList<>();
+        userIds.add(groupInfo.getGroupId());
+        custosSharingImpl.shareEntity(groupInfo.getTenantId(),
+                dataProduct.getDataProductId(), permission.name(), userIds, true, Constants.GROUP, null);
+    }
+
+    @Override
+    public void revokePermissionFromGroup(GroupInfo groupInfo, DataProduct dataProduct, Permission permission) throws CustosSharingException {
+        List<String> userIds = new ArrayList<>();
+        userIds.add(groupInfo.getGroupId());
+        custosSharingImpl.revokePermission(groupInfo.getTenantId(),
+                dataProduct.getDataProductId(), permission.name(), userIds);
+    }
+
+    @Override
+    public boolean hasPublicAccess(DataProduct dataProduct, Permission permission) throws CustosSharingException{
+        return custosSharingImpl.userHasAccess(dataProduct.getTenantId(), dataProduct.getDataProductId(), permission.name(),
+                PUBLIC_ACCESS_GROUP);
+    }
+
+    @Override
+    public void grantPublicAccess(DataProduct dataProduct, Permission permission) throws CustosSharingException{
+
+        //TODO: create PUBLIC GROUP If not exists
+        List<String> userIds = new ArrayList<>();
+        userIds.add(PUBLIC_ACCESS_GROUP);
+        custosSharingImpl.shareEntity(dataProduct.getTenantId(),
+                dataProduct.getDataProductId(), permission.name(), userIds, true, Constants.GROUP, null);
 
     }
 
     @Override
-    public void grantPermissionToGroup(GroupInfo groupInfo, DataProduct dataProduct, Permission permission) {
-
-    }
-
-    @Override
-    public void revokePermissionFromGroup(GroupInfo groupInfo, DataProduct dataProduct, Permission permission) {
-
-    }
-
-    @Override
-    public boolean hasPublicAccess(DataProduct dataProduct, Permission permission) {
-        return false;
-    }
-
-    @Override
-    public void grantPublicAccess(DataProduct dataProduct, Permission permission) {
-
-    }
-
-    @Override
-    public void revokePublicAccess(DataProduct dataProduct, Permission permission) {
-
+    public void revokePublicAccess(DataProduct dataProduct, Permission permission) throws CustosSharingException{
+        List<String> userIds = new ArrayList<>();
+        userIds.add(PUBLIC_ACCESS_GROUP);
+        custosSharingImpl.revokePermission(dataProduct.getTenantId(),
+                dataProduct.getDataProductId(), permission.name(), userIds);
     }
 }
