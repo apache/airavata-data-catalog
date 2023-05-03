a65085d5535191f8641837974812de165612323f
Marcus Christie <machristie@apache.org>
2023-05-03 09:47:34 -0400

    Don't start a web server port, unneeded

--- data-catalog-api/server/service/src/main/java/org/apache/airavata/datacatalog/api/DataCatalogApiServiceApplication.java
+++ data-catalog-api/server/service/src/main/java/org/apache/airavata/datacatalog/api/DataCatalogApiServiceApplication.java
@@ -5,9 +5,10 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.apache.airavata.datacatalog.api.sharing.SharingManager;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Primary;
@@ -24,7 +25,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class DataCatalogApiServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DataCatalogApiServiceApplication.class, args);
+        new SpringApplicationBuilder(DataCatalogApiServiceApplication.class).web(WebApplicationType.NONE).run(args);
     }
 
     @Bean
